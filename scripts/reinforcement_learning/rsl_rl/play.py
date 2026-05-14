d25398d59056206858370dd5d41a2a29f2c2a707
Jinhoon Lee <kjjh1126@sogang.ac.kr>
2026-05-14 13:21:18 +0900

    env unwrap

--- scripts/reinforcement_learning/rsl_rl/play.py
+++ scripts/reinforcement_learning/rsl_rl/play.py
@@ -186,8 +186,8 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
             actions = policy(obs)
             # env stepping
             obs, _, _, _ = env.step(actions)
-            imu_acc = env.scene["imu"].data.root_lin_acc_b
-            all_forces = env.scene["robot"].data.joint_force
+            imu_acc = env.unwrapped.scene["imu"].data.root_lin_acc_b
+            all_forces = env.unwrapped.scene["robot"].data.joint_force
             left_knee_moment = all_joint_forces[:, 11, 3:]
             right_knee_moment = all_joint_forces[:, 12, 3:]
             print(imu_acc)
