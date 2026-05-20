bedb3b8b6635d74cd8d5f61270c3f01e052f215e
Jinhoon Lee <kjjh1126@sogang.ac.kr>
2026-05-20 13:59:49 +0900

    debug shape

--- scripts/reinforcement_learning/rsl_rl/play.py
+++ scripts/reinforcement_learning/rsl_rl/play.py
@@ -186,8 +186,9 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
             actions = policy(obs)
             # env stepping
             obs, _, _, _ = env.step(actions)
-            
+
             torso_accel = env.unwrapped.scene["imu_torso"].data.lin_acc_b
+            print(shape(torso_accel))
             l_thigh_accel = env.unwrapped.scene["imu_l_thigh"].data.lin_acc_b
             r_thigh_accel = env.unwrapped.scene["imu_r_thigh"].data.lin_acc_b
 
